105e4fd3b0fea5b113788e6ad95a831cc31f7603
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-07-27 04:10:50 -0700

    add a table content message

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 //create a function generator message radonm
 function randomMessage(){
 
+    //create a table content message
+        var message = ['Bonjour', 'Hello', 'Mbote', 'Salut', 'Djambo'];
+    
     }
     
     console.log(indexMessage)
